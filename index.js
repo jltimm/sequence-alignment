08b3e325746ff286b5a53f4334358beb1b6701e3
jltimm <jacobltimm@gmail.com>
2019-05-06 19:46:39 -0500

    Barebone commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function SeqAlign() {}
+
+module.exports = SeqAlign;
+
+SeqAlign.prototype.nw = function() {
+  console.log("Hello, world!");
+}
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var SeqAlign = require('../index.js');
+
+function runTests() {
+  var sa = new SeqAlign();
+  sa.nw();
+}
+
+runTests();
